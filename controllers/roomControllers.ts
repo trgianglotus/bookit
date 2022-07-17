7a1beddcb54bfc7055c656012f8f9dc4954f52ee
Nguyen Giang <hoangtruonggiangnguyen@gmail.com>
2022-07-17 15:53:04 +0800

    create room

--- controllers/roomControllers.ts
+++ controllers/roomControllers.ts
@@ -1,21 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import rooms from '../models/rooms'
+import Room from '../models/rooms'
 
-const allRooms = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({
-    success: true,
-    message: 'All rooms',
-  })
+const allRooms = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const rooms = await Room.find()
+    res.status(200).json({ success: true, count: rooms.length, rooms })
+  } catch (error: any) {
+    res.status(400).json({ succes: false, error: error.message })
+  }
 }
 
 // Create new room => /api/rooms
 const newRoom = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
+    const room = await Room.create(req.body)
+    res.status(200).json({ sucess: true, room })
   } catch (error: any) {
     res.status(400).json({ success: false, error: error.message })
   }
-  const room = await rooms.create(req.body)
-  res.status(200).json({ sucess: true, room })
 }
 
 export { allRooms, newRoom }
