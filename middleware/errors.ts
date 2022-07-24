86a38b886638ce0b5713529ae9f8bb5097ca6deb
Nguyen Giang <hoangtruonggiangnguyen@gmail.com>
2022-07-24 16:38:21 +0700

    format error messages

--- middleware/errors.ts
+++ middleware/errors.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import ErrorHandler from '../utils/errorHandler'
 
 const onError = (err: any, req: NextApiRequest, res: NextApiResponse) => {
   err.statusCode = err.statusCode || 500
@@ -6,6 +7,18 @@ const onError = (err: any, req: NextApiRequest, res: NextApiResponse) => {
   let error = { ...err }
 
   error.message = err.message
+
+  // Wrong mongoose Object ID Error
+  if (err.name == 'CastError') {
+    const message = `Resource not found. Invalid: ${err.path}`
+    error = new ErrorHandler(message, 400)
+  }
+
+  // Handling mongoose Validation Error
+  if (err.name == 'ValidationError') {
+    const message = Object.values(err.errors).map((value: any) => value.message)
+    error = new ErrorHandler(message, 400)
+  }
 
   res.status(err.statusCode).json({
     success: false,
